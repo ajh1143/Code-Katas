e557998c3f177d28035c6d6b96007ef616fa0587
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-09-05 13:44:26 -0700

    Create SpinWords_Test.py

---
+++ Spin_Words/SpinWords_Test.py
@@ -0,0 +1,4 @@
+test.assert_equals(spin_words("Welcome"), "emocleW")
+test.assert_equals(spin_words( "Hey fellow warriors" ), "Hey wollef sroirraw") 
+test.assert_equals(spin_words( "This is a test"),  "This is a test" )
+test.assert_equals(spin_words( "This is another test"), "This is rehtona test")
