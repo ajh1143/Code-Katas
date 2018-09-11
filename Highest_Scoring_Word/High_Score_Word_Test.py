1dd1dbe9ededae0635db9c0f5f06e873693767fb
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-09-11 14:54:39 -0700

    Create High_Score_Word_Test.py

---
+++ Highest_Scoring_Word/High_Score_Word_Test.py
@@ -0,0 +1,3 @@
+test.assert_equals(high('man i need a taxi up to ubud'), 'taxi')
+test.assert_equals(high('what time are we climbing up the volcano'), 'volcano')
+test.assert_equals(high('take me to semynak'), 'semynak')
