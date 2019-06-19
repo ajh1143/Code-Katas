e368734c5d48121583921795b8bacef3a91d6519
Andrew Holt <ajh.206@outlook.com>
2019-06-19 14:16:19 -0700

    Create CamelConversionTest.py
    Tests script

---
+++ CamelConversion/CamelConversionTest.py
@@ -0,0 +1,8 @@
+test.describe("Testing function to_camel_case")
+test.it("Basic tests")
+test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
+test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
+test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
+test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
+test.assert_equals(to_camel_case('jon-of-the_Snow'), 'jonOfTheSnow', "You know nothing, try again")
+test.assert_equals(to_camel_case('Hod-o-r'), 'HodOR', "Hold the door and gtfo")
